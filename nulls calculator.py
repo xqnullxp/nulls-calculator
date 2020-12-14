d2aeef76910caeadbd3046b3a321086ba084ab24
null <75458417+xqnullxp@users.noreply.github.com>
2020-12-14 00:55:58 -0600

    Add files via upload

---
+++ nulls calculator.py
@@ -0,0 +1,29 @@
+#wrote by null
+#kys
+
+print('pick sum fag')
+print('1~add')
+print('2~subtract')
+print('3~multipy')
+print('4~divide')
+
+option = input('Enter choice(1/2/3/4): ')
+
+if option in ('1', '2', '3', '4'):
+    shit1 = int(input('enter ur first shit: '))
+    shit2 = int(input('enter ur second shit: '))
+
+if option == "1":
+    print(shit1 + shit2)
+
+elif option == '2':
+    print(shit1 - shit2)
+
+elif option == '3':
+    print(shit1 * shit2)
+
+elif option == '4':
+    print(shit1 / shit2)
+
+else:
+    print('error you fucking dumb cunt...')    
